1e07518772286708f5c7fd666f3997c7f6d58fef
AtoKashinoki <atokashinoki@gmail.com>
2023-07-05 16:27:41 +0900

    初期状態

---
+++ LifeGame_main.py
@@ -0,0 +1,23 @@
+"""
+
+    Life game program
+
+"""
+# import module
+import sys
+import pygame as pg
+
+
+class Application:
+    """
+        LifeGame application class
+    """
+
+    def __init__(self):
+        return
+
+
+if __name__ == '__main__':
+    app = Application()
+    pg.quit()
+    sys.exit()
